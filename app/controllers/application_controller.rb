3e54c3345004d20dcddfba5c6321b70bb34d1fae
Lewis Reid <lreidwilliams@gmail.com>
2023-07-08 19:37:47 +1000

    fix: added error response for follow and unfollow requests when users are missing
    Responds with 404 when ActiveRecord fails to find records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+  private
+
+  def handle_not_found(_exception)
+    head :not_found
+  end
 end
--- spec/requests/follows_request_spec.rb
+++ spec/requests/follows_request_spec.rb
@@ -4,6 +4,20 @@ RSpec.describe "Follows Requests" do
   let(:user) { User.create!(name: "Kat") }
   let(:followed_user) { User.create!(name: "Ana") }
 
+  shared_examples "missing users are handled" do
+    it "responds with error when user is missing" do
+      user.destroy
+      subject
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "responds with error when followed user is missing" do
+      followed_user.destroy
+      subject
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "POST /users/:user_id/follows" do
     subject { post(user_follows_path(user_id: user.id), params: { followed_user_id: followed_user.id }) }
 
@@ -15,12 +29,12 @@ RSpec.describe "Follows Requests" do
     it "follows the provided user" do
       expect { subject }.to change { user.following?(followed_user) }.from(false).to(true)
     end
+
+    it_behaves_like "missing users are handled"
   end
 
   describe "DELETE /users/:user_id/follows/:followed_user_id" do
     subject { delete(user_follow_path(user_id: user.id, followed_user_id: followed_user.id)) }
-
-    before { user.follow(followed_user) }
 
     it "responds with success" do
       subject
@@ -28,7 +42,10 @@ RSpec.describe "Follows Requests" do
     end
 
     it "unfollows the provided user" do
+      user.follow(followed_user)
       expect { subject }.to change { user.following?(followed_user) }.from(true).to(false)
     end
+
+    it_behaves_like "missing users are handled"
   end
 end
